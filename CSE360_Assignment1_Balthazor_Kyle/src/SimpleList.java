78b9d818bc9676d4b3c26033355af8d55ef0728a
Kyle <kylebalthazor1@gmail.com>
2020-02-27 23:08:47 -0700

    Second commit for the second section of the assignment. Added first, last, append, and size.

--- CSE360_Assignment1_Balthazor_Kyle/src/SimpleList.java
+++ CSE360_Assignment1_Balthazor_Kyle/src/SimpleList.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 /**
@@ -10,6 +11,7 @@ public class SimpleList
 {
 	private ArrayList<Integer> List; 
 	private int count;
+	private int capacity;
 	
 	/**
 	 * Constructor for the SimpleList Class, creates an array of size 10 and sets the integer 
@@ -20,17 +22,19 @@ public class SimpleList
 	{
 		this.List = new ArrayList<Integer>(10);
 		this.count = 0;
+		this.capacity = 10;
 	}
 	
 	/**
 	 * Add the parameter to the list at the beginning, move all other integers in the list over. If the list is full,
-	 * then the last element is removed from the list. Also increments the count by 1, unless the count is already at 10.
+	 * then the last element is removed from the list. Also increments the count by 1, unless the count is already at 10. 
+	 * If the count is greater than the size of the list then increase the size by 50%.
 	 * @param newInteger Specifies the integer that is to be added to the array.
 	 */
 	
 	public void add(int newInteger) 
 	{
-		if (this.count < 10) 
+		if (this.count <= 10) 
 		{
 			if (this.count == 0) 
 			{  // If it is the first Number in the array just add the number.
@@ -46,10 +50,16 @@ public class SimpleList
 				{
 					holderArray[index] = this.List.get(index-1);
 				}
-				this.List.clear();
 				for(int position = 0; position < holderArray.length; position++)
 				{
-					this.List.set(position, holderArray[position]);
+					if (position+1 < this.count)
+					{
+						this.List.set(position, holderArray[position]);
+					}
+					else
+					{
+						this.List.add(position, holderArray[position]);
+					}
 				}
 			}
 		}
@@ -57,6 +67,7 @@ public class SimpleList
 		{
 			this.count++;
 			this.List.ensureCapacity((this.List.size()/2)+this.List.size());
+			this.capacity = this.List.size()/2+this.List.size();
 			
 			int holderArray[] = new int[count];
 			holderArray[0] = newInteger;
@@ -66,17 +77,24 @@ public class SimpleList
 				holderArray[index] = this.List.get(index-1);
 			}
 			
-			this.List.clear();
 			for(int position = 0; position < holderArray.length; position++)
 			{
-				this.List.set(position, holderArray[position]);
+				if (position+1 < this.count)
+				{
+					this.List.set(position, holderArray[position]);
+				}
+				else
+				{
+					this.List.add(position, holderArray[position]);
+				}
 			}
 		}
 	}
 	
 	/**
 	 * This function finds if a certain value is in the list, and if found it is removed and the rest of the 
-	 * list is shifted down. Also decreases the count by 1 if the value is found.
+	 * list is shifted down. Also decreases the count by 1 if the value is found. If the list has more than 25% 
+	 * null index's then remove the null indexs.
 	 * @param removeValue
 	 */
 	
@@ -99,30 +117,17 @@ public class SimpleList
 				}
 				else 
 				{
-					int holderArray[] = new int[this.count -1];
-					System.arraycopy(List, 0, holderArray, 0, returnIndex);
-					System.arraycopy(List, returnIndex+1 , holderArray, returnIndex, this.count-returnIndex-1);
+					this.List.remove(returnIndex);
 					this.count--;
-					for(int position = 0; position < holderArray.length; position++)
-					{
-						this.List.set(position, holderArray[position]);
-					}
 				}
 			}
 			
-			int numberNull = 0;
-			for(int index = 0; index < this.count; index++)
-			{
-				if(this.List.get(index) == null)
-				{
-					numberNull++;
-				}
-			}
-			if(numberNull >= this.List.size()*.25 && numberNull != this.count)
+			
+			if(this.capacity >= this.List.size()*.25 && this.capacity != this.count)
 			{
 				this.List.trimToSize();
 			}
-			else if (numberNull >= this.List.size()*.25 && numberNull == this.count)
+			else if (this.capacity >= this.List.size()*.25 && this.capacity == this.count)
 			{
 				this.List = new ArrayList<Integer>(1);
 			}
@@ -156,6 +161,68 @@ public class SimpleList
 		}
 	return returnIndex;		
 	}
+	/**
+	 * Function to add a new integer to the last index of the list. If the List is full then increase the size by 50%.
+	 * @param newInteger The new value that is to be placed at the last index of the list.
+	 */
+	
+	public void append(int newInteger)
+	{
+		if(this.List.size() == this.count)
+		{
+			this.List.ensureCapacity((this.List.size()/2)+this.List.size());
+			this.List.add(newInteger);
+			this.count++;
+		}
+		else
+		{
+			this.List.add(newInteger);
+			this.count++;
+		}
+		
+	}
+	/**
+	 * Function to return the first element in the List, else returns -1.
+	 * @return Returns as -1 if there is nothing in the list, else returns the integer.
+	 */
+	public int first()
+	{
+		int returnValue = -1;
+		if(this.count > 0)
+		{
+			returnValue = this.List.get(0);
+		}
+		
+		return returnValue;
+	}
+	/**
+	 * Function to return the last element in the List, else returns -1.
+	 * @return Returns as -1 if there is nothing in the list, else returns the integer.
+	 */
+	
+	public int last()
+	{
+		int returnValue = -1;
+		if(this.count > 0)
+		{
+			returnValue = this.List.get(this.count);
+		}
+		return returnValue;
+	}
+	
+	/**
+	 * Function to determine how many elements could be in the List, i.e. the capacity.
+	 * @return	Returns the capacity of the List 
+	 * @throws NoSuchFieldException Exception if the field does not exist, no list.
+	 * @throws SecurityException Exception for if the security denies it.
+	 * @throws IllegalArgumentException Exception for the argument not being there.
+	 * @throws IllegalAccessException Exception for if you are unable to access the data.
+	 */
+	
+	public int size() 
+	{
+		return this.capacity;
+	}
 	
 	/**
 	 * Returns the list as a string, each element is separated by a space with no space after the last value.
@@ -181,17 +248,7 @@ public class SimpleList
 	{
 		SimpleList newList = new SimpleList();
 		
-		newList.add(4);
-		newList.add(4);
-		newList.add(4);
-		newList.add(4);
-		newList.add(4);
-		newList.add(4);
-		newList.add(4);
-		newList.add(4);
-		newList.add(4);
-		newList.add(4);
-		newList.add(4);
+		
 		newList.add(4);
 		newList.add(4);
 		newList.remove(4);
